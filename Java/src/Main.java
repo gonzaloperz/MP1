079dd09f9d184000beaa5f204e261d5a0f720cd9
Gonzalo Pérez <g.perezr.2019@alumnos.urjc.es>
2023-04-20 17:20:37 +0200

    Changes by paula

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        Appcontroller appcontroller= new Appcontroller();
+        appcontroller.start();
     }
 }
