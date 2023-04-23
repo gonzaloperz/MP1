b5c641207922a8f2221adbc3b53e2f0d83c00d40
Gonzalo Pérez <g.perezr.2019@alumnos.urjc.es>
2023-04-23 13:23:19 +0200

    viernes noche

---
+++ Java/src/Controladores/Pantalla.java
@@ -0,0 +1,21 @@
+package Controladores;
+
+import java.util.Scanner;
+
+public abstract class Pantalla {
+
+
+    public static void imprimir(String texto){
+        System.out.println(texto);
+    }
+    public static int pedirenteros(String texto){
+        imprimir(texto);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+    public  static String pedircadena(String texto){
+        imprimir(texto);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
